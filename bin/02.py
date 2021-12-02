6df90dde5a6c21c41745baf2ee76de93f3956646
Johann Dickson <johann.dickson@gmail.com>
2021-12-02 22:19:53 +1300

    pt2.2

--- bin/02.py
+++ bin/02.py
@@ -25,5 +25,20 @@ def part1(instructions):
     return pos['x'] * pos['y']
 
 
+def part2(instructions):
+    pos = {'x': 0, 'y': 0, 'aim': 0}
+    for i in instructions:
+        direction, distance = i.split(' ')
+        if direction == "forward":
+            pos['x'] += int(distance)
+            pos['y'] += pos['aim']*int(distance)
+        elif direction == "up":
+            pos['aim'] -= int(distance)
+        elif direction == "down":
+            pos['aim'] += int(distance)
+    return pos['x'] * pos['y']
+
+
 if __name__ == '__main__':
     print("Part 1:", part1(myInput))
+    print("Part 2:", part2(myInput))
