a04eba97e5291689fd52db2bc2e077f443dd1304
Johann Dickson <johann.dickson@gmail.com>
2021-12-05 18:53:10 +1300

    pt5.2

--- bin/05.py
+++ bin/05.py
@@ -12,25 +12,22 @@ with open( os.path.join(os.path.dirname(__file__), inputfile) ) as f:
     myInput = [x.strip() for x in f.readlines()]
 
 
-def findVents(vents):
+def findVents(vents, diagonals=False):
     maxX = 0
     maxY = 0
     simpleVents = []
+    complexVents = []
     for l in vents:
         start, end = l.split(' -> ')
-        startx,starty = start.split(',')
-        endx,endy = end.split(',')
+        startx, starty = [int(z) for z in start.split(',')]
+        endx, endy = [int(z) for z in end.split(',')]
+        maxX = max([maxX, startx, endx])
+        maxY = max([maxY, starty, endy])
 
         if startx == endx or starty == endy:
-            startx = int(startx)
-            endx = int(endx)
-            starty = int(starty)
-            endy = int(endy)
-
             simpleVents.append([startx, starty, endx, endy])
-
-            maxX = max([maxX, startx, endx])
-            maxY = max([maxY, starty, endy])
+        else:
+            complexVents.append([startx, starty, endx, endy])
 
     ventGrid = []
     for i in range(maxY+1):
@@ -43,6 +40,20 @@ def findVents(vents):
             for x in range(min(vent[0], vent[2]), max(vent[0], vent[2])+1):
                 ventGrid[y][x] += 1
 
+    if diagonals:
+        for vent in complexVents:
+            xdir = 1
+            ydir = 1
+            if vent[0] > vent[2]:
+                xdir = -1
+            if vent[1] > vent[3]:
+                ydir = -1
+            for (y, x) in zip(
+                range(vent[1], vent[3]+ydir, ydir),
+                range(vent[0], vent[2]+xdir, xdir)
+                ):
+                    ventGrid[y][x] += 1
+
     return ventGrid
 
 
@@ -54,5 +65,14 @@ def part1(vents):
     return score
 
 
+def part2(vents):
+    outvents = findVents(vents, diagonals=True)
+    score = 0
+    for y in outvents:
+        score += sum([1 for x in y if x >1])
+    return score
+
+
 if __name__ == '__main__':
     print("Part 1:", part1(myInput))
+    print("Part 2:", part2(myInput))
