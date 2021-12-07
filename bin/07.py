8dc109ebcf21d3e0ff5243ba1fcbbc2a723b06a8
Johann Dickson <johann.dickson@gmail.com>
2021-12-07 18:55:29 +1300

    pt7.1

---
+++ bin/07.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+import os
+from collections import Counter
+
+
+testfile = "../test/07_1.txt"
+with open( os.path.join(os.path.dirname(__file__), testfile) ) as f:
+    testInput = [int(x) for x in f.read().strip().split(',')]
+
+inputfile = "../input/07.txt"
+with open( os.path.join(os.path.dirname(__file__), inputfile) ) as f:
+    myInput = [int(x) for x in f.read().strip().split(',')]
+
+
+def part1(crabs):
+    crabs = Counter(crabs)
+    mn = min(crabs)
+    mx = max(crabs)
+    bestFuel = None
+    for i in range(mn, mx+1):
+        fuelConsumption = sum([abs(i-p)*c for p,c in crabs.items()])
+        if bestFuel == None or fuelConsumption < bestFuel:
+            bestFuel = fuelConsumption
+    return bestFuel
+
+if __name__ == '__main__':
+    print("Part 1:", part1(myInput))
