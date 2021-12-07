6c6c507048394e6783e46c124d607affe5416775
Johann Dickson <johann.dickson@gmail.com>
2021-12-07 19:02:57 +1300

    pt7.2

--- bin/07.py
+++ bin/07.py
@@ -24,5 +24,19 @@ def part1(crabs):
             bestFuel = fuelConsumption
     return bestFuel
 
+
+def part2(crabs):
+    crabs = Counter(crabs)
+    mn = min(crabs)
+    mx = max(crabs)
+    bestFuel = None
+    for i in range(mn, mx+1):
+        fuelConsumption = sum([sum(range(abs(i-p)+1))*c for p,c in crabs.items()])
+        if bestFuel == None or fuelConsumption < bestFuel:
+            bestFuel = fuelConsumption
+    return bestFuel
+
+
 if __name__ == '__main__':
     print("Part 1:", part1(myInput))
+    print("Part 2:", part2(myInput))
