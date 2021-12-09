e6b61514b6ee1249826d5811e7b1a389a6c21094
Johann Dickson <johann.dickson@gmail.com>
2021-12-09 18:49:10 +1300

    pt9.2

--- bin/09.py
+++ bin/09.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import os
+from math import prod
 
 
 testfile = "../test/09_1.txt"
@@ -35,6 +36,32 @@ def find_low_points(grid):
     return lowpoints
 
 
+checked_points = set([])
+def find_basin_neighbours(grid, point):
+    global checked_points
+
+    neighbours = 0
+    for pos in [
+        (point[0]-1, point[1]),
+        (point[0]+1, point[1]),
+        (point[0], point[1]-1),
+        (point[0], point[1]+1),
+        ]:
+        if pos not in checked_points and grid[ pos[0] ][ pos[1] ] < 9:
+            checked_points.add(pos)
+            neighbours += 1
+            neighbours += find_basin_neighbours(grid, pos)
+    return neighbours
+
+
+def find_basins(grid, lowpoints):
+    basins = []
+    for p in lowpoints:
+        basin = find_basin_neighbours(grid, p)
+        basins.append(basin)
+    return basins
+
+
 def part1(tubes):
     tubes = input_to_grid(tubes)
     lp = find_low_points(tubes)
@@ -43,5 +70,13 @@ def part1(tubes):
     return sum(score)
 
 
+def part2(tubes):
+    tubes = input_to_grid(tubes)
+    lp = find_low_points(tubes)
+    basins = find_basins(tubes, lp)
+    return prod(sorted(basins)[-3:])
+
+
 if __name__ == '__main__':
     print("Part 1:", part1(myInput))
+    print("Part 2:", part2(myInput))
