343364168cf473c5dd4f904b988108d75120a228
Johann Dickson <johann.dickson@gmail.com>
2021-12-15 14:07:07 +1300

    pt14.2 smarter

--- bin/14.py
+++ bin/14.py
@@ -21,23 +21,26 @@ def part1(polymers, iterations):
         pair, new = r.split(' -> ')
         changes.append((pair, new))
 
-    for _ in range(iterations):
-        inserts = {}
-        for (pair, new) in changes:
-            if poly.count(pair):
-                try:
-                    index = 0
-                    while poly.index(pair, index) > -1:
-                        index = poly.index(pair, index)+1
-                        inserts[index] = new
-                except ValueError: pass
-
-        for i in sorted(inserts.keys(), reverse=True):
-            poly = poly[:i] + inserts[i] + poly[i:]
-
+    pair_counter = Counter()
     c = Counter(poly)
+    for i in range(len(poly)-1):
+        if poly[i:i+2] not in pair_counter:
+            pair_counter[ poly[i:i+2] ] = 1
+        else:
+            pair_counter[ poly[i:i+2] ] += 1
+
+    for _ in range(iterations):
+        before_split = pair_counter.copy()
+        for (pair, new) in changes:
+            if pair in pair_counter:
+                pair_counter[pair] -= before_split[pair]
+                pair_counter[pair[0]+new] += before_split[pair]
+                pair_counter[new+pair[1]] += before_split[pair]
+                c[new] += before_split[pair]
+
     return max(c.values()) - min(c.values())
 
 
 if __name__ == '__main__':
     print("Part 1:", part1(myInput, 10))
+    print("Part 2:", part1(myInput, 40))
