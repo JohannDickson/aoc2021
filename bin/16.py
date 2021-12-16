25dbe4dd0a2295665f5320a8cdc4966c0a6dd05d
Johann Dickson <johann.dickson@gmail.com>
2021-12-16 22:11:12 +1300

    pt16.1

--- bin/16.py
+++ bin/16.py
@@ -9,59 +9,7 @@ with open( os.path.join(os.path.dirname(__file__), testfile) ) as f:
 
 inputfile = "../input/16.txt"
 with open( os.path.join(os.path.dirname(__file__), inputfile) ) as f:
-    myInput = [x.strip() for x in f.readlines()]
-
-subPacket = None
-versionSum = 0
-def readPacket(packet):
-    global versionSum, subPacket
-
-    version = int(packet[:3], 2)
-    versionSum+=version
-    type_id = int(packet[3:6], 2)
-    print("Version", version, type_id)
-
-    if type_id == 4:
-        number_chunk = list(packet[6:])
-        the_number = ''
-        is_last = None
-        while is_last != '0':
-            is_last = number_chunk.pop(0)
-            if is_last == '1':
-                the_number += ''.join([number_chunk.pop(0) for x in range(4)])
-            elif is_last == '0':
-                the_number += ''.join([number_chunk.pop(0) for x in range(4)])
-
-        if subPacket is not None:
-            subPacket = ''.join(number_chunk)
-            print("REMAINDER", subPacket)
-
-        value = int(the_number, 2)
-
-    else:
-        length_type_id = int(packet[6:7], 2)
-        contents = packet[7:]
-        print("Operator", length_type_id)
-        if  length_type_id == 0:
-            total_length_in_bits = int(contents[:15], 2)
-            sub_packet = contents[15:15+total_length_in_bits]
-            # Identify multiple sub packets
-            subPacket = sub_packet
-            while subPacket is not None:
-                print("Sub", total_length_in_bits, sub_packet)
-                print(readPacket(sub_packet))
-
-        elif length_type_id == 1:
-            number_of_sub_packets = int(contents[:11], 2)
-            sub_packet=contents[11:]
-            print("Sub", number_of_sub_packets, sub_packet)
-            for i in range(number_of_sub_packets):
-                print(readPacket(sub_packet))
-                sub_packet = subPacket
-
-        value = 0
-
-    return value
+    myInput = f.read().strip()
 
 
 hexmap = {
@@ -83,20 +31,60 @@ hexmap = {
     "F": "1111",
 }
 
-def part1(hexinput):
-    print(hexinput)
-    binstring = ''.join([hexmap[c] for c in hexinput])
-    val = readPacket(binstring)
 
-    return val
+versionSum = 0
+def readPacket(packet):
+    global versionSum
+
+    version = int(packet[:3], 2)
+    type_id = int(packet[3:6], 2)
+
+    versionSum+=version
+
+    if type_id == 4:
+        number_chunk = list(packet[6:])
+
+        the_number = ''
+        is_last = None
+
+        while is_last != '0':
+            is_last = number_chunk.pop(0)
+            if is_last == '1':
+                the_number += ''.join([number_chunk.pop(0) for x in range(4)])
+            elif is_last == '0':
+                the_number += ''.join([number_chunk.pop(0) for x in range(4)])
+
+        return ((version, type_id, int(the_number, 2)), ''.join(number_chunk))
+
+    else:
+        length_type_id = int(packet[6:7], 2)
+        contents = packet[7:]
+        subs = []
+
+        if length_type_id == 0:
+            total_length_in_bits = int(contents[:15], 2)
+            nextpacket = contents[15:15+total_length_in_bits]
+            while nextpacket != '':
+                (res, nextpacket) = readPacket(nextpacket)
+                subs.append(res)
+            return ((version, type_id, subs), contents[15+total_length_in_bits:])
+
+        elif length_type_id == 1:
+            number_of_sub_packets = int(contents[:11], 2)
+            nextpacket=contents[11:]
+            for i in range(number_of_sub_packets):
+                (res, nextpacket) = readPacket(nextpacket)
+                subs.append(res)
+            return ((version, type_id, subs), nextpacket)
+
+
+def part1(hexinput):
+    global versionSum
+    versionSum = 0
+    binstring = ''.join([hexmap[c] for c in hexinput])
+    readPacket(binstring)
+    return versionSum
 
 
 if __name__ == '__main__':
-    for t in testInput:
-        print(part1(t))
-        versionSum = 0
-        # break
-    expected1 = part1(t[-1])
-    print(f"Test part 1 ({expected1}):", test1)
-    if test1 == expected1:
-        print("Part 1:", part1(myInput))
+    print("Part 1:", part1(myInput))
