5007fc3d0af4ef9a3a455dd553ff23fcd517a3e2
Johann Dickson <johann.dickson@gmail.com>
2021-12-20 18:34:31 +1300

    ptwaythere

--- bin/20.py
+++ bin/20.py
@@ -2,32 +2,120 @@
 
 import os
 import json
-from math import ceil, floor
-
+from collections import defaultdict
 
 testfile = "../test/20_1.txt"
 with open( os.path.join(os.path.dirname(__file__), testfile) ) as f:
-    testInput = [x.strip() for x in f.readlines()]
+    testInput = f.read().strip()
 
 inputfile = "../input/20.txt"
 with open( os.path.join(os.path.dirname(__file__), inputfile) ) as f:
-    myInput = [x.strip() for x in f.readlines()]
+    myInput = f.read().strip()
 
 
-def find_deepest(numbers):
-    return None
+def print_grid(grid):
+    out = ''
+    for y in grid:
+        out += ''.join([str(x) for x in y])+'\n'
+    print(out)
 
 
-def part1(numbers):
+def updateGrid(grid, algo):
+    new_grid = grid
+
+    # from pprint import pprint
+    # pprint(grid)
+    for i in list(grid.keys()):
+        for j in list(grid[i].keys()):
+            substr = ''
+
+            try:
+                substr+=grid[i-1][j-1]
+            except:
+                grid[i-1][j-1]='0'
+                substr+='0'
+            try:
+                substr+=grid[i-1][j]
+            except:
+                grid[i-1][j]='0'
+                substr+='0'
+            try:
+                substr+=grid[i-1][j+1]
+            except:
+                grid[i-1][j+1]='0'
+                substr+='0'
+
+            try:
+                substr+=grid[i][j-1]
+            except:
+                grid[i][j-1]='0'
+                substr+='0'
+            substr+=grid[i][j]
+            try:
+                substr+=grid[i][j+1]
+            except:
+                grid[i][j+1]='0'
+                substr+='0'
+
+            try:
+                substr+=grid[i+1][j-1]
+            except:
+                grid[i+1][j-1]='0'
+                substr+='0'
+            try:
+                substr+=grid[i+1][j]
+            except:
+                grid[i+1][j]='0'
+                substr+='0'
+            try:
+                substr+=grid[i+1][j+1]
+            except:
+                grid[i+1][j+1]='0'
+                substr+='0'
+
+            bin_number = int(substr, 2)
+            new_number = algo[bin_number]
+            new_grid[i][j] = new_number
+
+    return new_grid
+
+
+def part1(pixels):
+    algo, image = pixels.split('\n\n')
+    algo = algo.replace('#', '1').replace('.', '0')
+
+    positions = defaultdict(dict)
+    grid = [y.strip() for y in image.strip().split('\n')]
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            if grid[y][x]=='#':
+                positions[y][x] = '1'
+            else:
+                positions[y][x] = '0'
+
+
     result = 0
-    for i in numbers:
-        print(json.loads(i))
-        reduce(json.loads(i))
+    for k in positions.keys():
+        result+= sum([int(x) for x in positions[k].values()])
+    print(result)
+    positions = updateGrid(positions,algo)
+    result = 0
+    for k in positions.keys():
+        result+= sum([int(x) for x in positions[k].values()])
+    print(result, 23)
+
+    positions = updateGrid(positions,algo)
+
+    result = 0
+    for k in positions.keys():
+        result+= sum([int(x) for x in positions[k].values()])
+    print(result, 35)
+
     return result
 
 
 if __name__ == '__main__':
-    expected1 = None
+    expected1 = 35
     test1 = part1(testInput)
     print("Test 1:", expected1, expected1==test1, test1)
     if expected1 == test1:
